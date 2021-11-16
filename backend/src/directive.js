1ee589f9d37c85f9b7d5d3847cc4639ba2cbfe3e
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2021-11-16 08:46:58 +0200

    deleting unneeded functions and files

--- backend/src/directive.js
+++ backend/src/directive.js
@@ -1,61 +1,17 @@
 const fs = require('fs');
 const shortid = require('shortid');
-
-// creating the json file with shorten url in user directory
-function createURLFile(longURL, shortURL, userDir) {
-  fs.writeFileSync(
-    `./backend/users/${userDir}/${shortURL}.json`,
-    JSON.stringify(creatURLObject(shortURL, longURL))
-  );
-}
-
-//Updating counter of clicks of each shorten url
-function updateCounter(shortURL, userDir) {
-  const path = `./backend/users/${userDir}/${shortURL}.json`;
-  const file = JSON.parse(fs.readFileSync(path));
-  file.Counter += 1;
-  fs.writeFileSync(
-    path,
-    JSON.stringify({
-      LongURL: file.LongURL,
-      ShortURL: shortURL,
-      Counter: file.Counter,
-      CreateTime: file.CreateTime,
-    })
-  );
-}
-
-//Creating object with url format
-function creatURLObject(shortURL, longURL) {
-  const newObject = {
-    LongURL: longURL,
-    ShortURL: shortURL,
-    Counter: 0,
-    CreateTime: creatingTime(),
-  };
-  return newObject;
-}
-
-// reading all the files in the user dir and returning an object of all shortens of user
-function readFiles(dirname) {
-  const dataArr = {};
-  const filesArr = fs.readdirSync(dirname);
-  for (const file of filesArr) {
-    let data = JSON.parse(fs.readFileSync(`${dirname}/${file}`));
-    dataArr[data.ShortURL] = data.LongURL;
-  }
-  return dataArr;
-}
+const URLShorten = require('./models/urlShorten');
 
 //Check if this shorten id been used before
 function urlCheck(url) {
-  const allUsers = fs.readdirSync(`./backend/users`);
-  for (const user of allUsers) {
-    if (fs.existsSync(`./backend/users/${user}/${url}.json`)) {
-      return true;
+  URLShorten.exists({ short_URL: { $regex: { url } } }, (err, result) => {
+    if (err) {
+      throw err;
     }
-  }
-  return false;
+    if (result) {
+      return result;
+    }
+  });
 }
 
 //Generate random short url
@@ -67,34 +23,6 @@ function randomURL() {
   return randomURL;
 }
 
-//Making create time by specific format
-function creatingTime() {
-  const today = new Date();
-  const date =
-    today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-  const time =
-    today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-  const dateTime = date + ' ' + time;
-  return dateTime;
-}
-
-//Search file by short URL and return long URL of this file
-function findFile(url) {
-  const allUsers = fs.readdirSync('./backend/users');
-  for (const user of allUsers) {
-    if (fs.existsSync(`./backend/users/${user}/${url}.json`)) {
-      const file = JSON.parse(
-        fs.readFileSync(`./backend/users/${user}/${url}.json`)
-      );
-      updateCounter(url, user);
-      return file.LongURL;
-    }
-  }
-}
-
 module.exports = {
-  createURLFile,
   randomURL,
-  readFiles,
-  findFile,
 };
